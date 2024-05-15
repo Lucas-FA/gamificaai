36f806c1d0487b82969e3f7b8080ba2e01438b59
Lucas-FA <lucasfreitasdeabreu@hotmail.com>
2024-05-15 17:26:23 -0300

    feat: Carrossel parte 2

--- script.js
+++ script.js
@@ -75,3 +75,49 @@ const mostrarSlideAnterior = () => {
     //Renderiza o slideAtual
     banner.classList.add(slides[slideAtual]);
 }
+
+const selecionarSlide = (indiceSlide) => {
+    //p/ cada item do array (remove o item da classe do banner)
+    slides.forEach(slide => banner.classList.remove(slide));
+    slideAtual = indiceSlide;
+    banner.classList.add(slides[indiceSlide]);
+}
+
+let listaCases = [
+    {
+        imagem: "https://unsplash.it/600/400?image=40",
+        descricao: "Uma empresa de tecnologia lança um desafio de gamificação onde os funcionários devem propor e implementar ideias inovadoras."
+    },
+
+    {
+        imagem: "https://unsplash.it/600/400?image=14",
+        descricao: "Uma empresa de consultoria cria uma narrativa interativa de gamificação para seu programa de treinamento."
+    },
+
+    {
+        imagem: "https://unsplash.it/600/400?image=666",
+        descricao: "Uma empresa de vendas implementa uma competição gamificada entre equipes que competem pelo topo do ranking."
+    },
+
+    {
+        imagem: "https://unsplash.it/600/400?image=28",
+        descricao: "Uma empresa promove o bem estar dos funcionários através de um desafio de gamificação de condicionamento físico."
+    }
+]
+
+const renderizarCases = () => {
+    let elementoLista = document.getElementById("lista-cards");
+
+    //Template Strings
+    let template = ""
+
+    listaCases.forEach(cardCase => {
+        template += `<div class="card">
+            <img src="${cardCase.imagem}" alt="">
+            <p>${cardCase.descricao}</p>
+            <button>Ver mais</button>
+        </div>`
+    })
+
+    elementoLista.innerHTML = template;
+}
